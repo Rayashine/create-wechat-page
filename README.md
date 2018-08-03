d1ca43dbf07d6ca08d2ccc862f3d5783bcd4850a
qianxuejia <mentoulv@JR2018>
2018-08-03 10:18:57 +0800

    The first commit .

---
+++ README.md
@@ -0,0 +1,27 @@
+# create-wechat-page
+
+the cli to create weixin mini-program page component
+
+## v1.0.0
+
+## support
+
+1. the cli to create weixin mini-program page component
+2. support create multiple components
+3. support `Scss`
+### Usage
+
+```
+npm i -g create-wechat-page
+
+mkpage App
+//you will create weixin mini-program page component named App
+
+## component details
+
+- [name].html
+- [name].js
+- [name].scss
+- [name].json
+
+
---
+++ bin/mkpage.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+'use strict'
+const createWechatPage = require('../lib/createWechatPage')
+const program = require('commander')
+
+function log (name) {
+  console.log(name)
+  console.log(`you will create weixin mini-program page component named ${name}`)
+}
+
+(function () {
+  let name
+
+  program
+    .version('1.2.0')
+    .parse(process.argv)
+
+  name = process
+      .argv[2]
+      .split(',')
+    log(name)
+
+  console.log(name)
+  createWechatPage(name)
+
+  console.log('done!')
+})()
