afefbaf9d28825a972d8999071d178945723bf89
qianxuejia <mentoulv@JR2018>
2018-08-03 12:43:16 +0800

    Fix a bug.

--- bin/mkpage.js
+++ bin/mkpage.js
@@ -2,21 +2,31 @@
 'use strict'
 const createWechatPage = require('../lib/createWechatPage')
 const program = require('commander')
-
+function parseVal (val) {
+  return val.split(',')
+}
 function log (name) {
   console.log(name)
-  console.log(`you will create weixin mini-program page component named ${name}`)
+  console.log(`you will create pure component named ${name}`)
 }
 
 (function () {
   let name
 
-  program.version('1.2.0').parse(process.argv)
+  program
+    .version('1.2.0')
+    .option('-p --pure [names]', 'use pure component', parseVal)
+    .parse(process.argv)
 
-  name = process.argv[1].split(',')
-
+  if (program.pure) {
+    name = program.pure
+  } else {
+    name = name || process
+      .argv[2]
+      .split(',')
     log(name)
-
+  }
+  
   console.log(name)
   createWechatPage(name)
 
